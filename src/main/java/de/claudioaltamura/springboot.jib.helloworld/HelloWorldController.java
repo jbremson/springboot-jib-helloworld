04737b9e94ae2e7456c5c61b36192e461ac0f15b
Claudio Altamura <claudio@claudioaltamura.de>
2020-01-03 16:37:08 +0100

    new: initial commit

---
+++ src/main/java/de/claudioaltamura/springboot.jib.helloworld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package de.claudioaltamura.springboot.jib.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(value = {"/helloworld"})
+	public String greet() {
+		return "Hello World!";
+	}
+
+}
